8ef5ed8a0a780f742eaecc26a0263e0d5f155352
Petteri Peltonen <petteri.peltonen@vtt.fi>
2025-02-26 16:11:43 +0200

    found a possible error prone way of using the ranges (see func3 in Test-newFieldRanges.C, not sure if that should be handled. Also provided an example of error handling. TODO: Write a freeFunction ensureEvaluated(Rng) which deduces the correct field type and constructs it. Use that in the error handling of the field operations: Supported<decltype(ensureEvaluated(rng)...., ..., op> )

--- unittest/Test-newFieldRanges.C
+++ unittest/Test-newFieldRanges.C
@@ -17,17 +17,24 @@
 namespace Foam{
 
 
+
 NewVolScalarField func1(const NewVolScalarField& f)
 {
     return plus(f, f);
 }
 
-auto func2(const NewVolScalarField& f)
+template<class Rng>
+auto func2(const Rng& f)
 {
     return plus(f, f);
 }
 
+auto func3(const NewVolScalarField& f)
+{
+    return plus(f, f);
 }
+
+} //Namesapce Foam
 
 
 TEST_CASE("Test-newFieldRanges")
@@ -39,16 +46,45 @@ TEST_CASE("Test-newFieldRanges")
 
 
 
-    NewVolScalarField p = toNewField(reader.read_scalarField("p"));
+    const NewVolScalarField p = toNewField(reader.read_scalarField("p"));
+    const NewVolVectorField U = toNewField(reader.read_vectorField("U"));
+
     SECTION("Implicit cast from Rng -> NewGeometricField")
     {
         CHECK(approxEqual(func1(p) , volScalarField(p + p)));
     }
-    SECTION("Implicit cast from Rng -> const NewGeometricField&")
+
+    SECTION("Implicit cast from Range -> const NewVolScalarField&")
+    {
+        auto rng1 = plus(p, p);
+        auto rng = func1(rng1);
+        CHECK(approxEqual(NewVolScalarField(rng), volScalarField((p + p) + (p + p))));
+    }
+
+    SECTION("No casts, take a range and return a range")
     {
         auto rng = func2(plus(p, p));
-        CHECK(approxEqual(NewVolScalarField(rng), volScalarField(p + p + p)));
+        CHECK(approxEqual(NewVolScalarField(rng), volScalarField((p + p) + (p + p))));
     }
+
+    
+    SECTION("Implicit cast from Range -> const NewVolScalarField& and return a range")
+    {
+        //TODO: This should work...
+        auto rng1 = plus(p, p);
+        auto rng = func3(rng1);
+        CHECK(approxEqual(NewVolScalarField(rng), volScalarField((p + p) + (p + p))));
+    }
+
+    SECTION("Error messages")
+    {
+        //volScalarField ret = p + p + U;
+
+        //NewVolScalarField ret2 = plus(plus(p, p), U);
+
+    }
+
+
 
     
 
